2a6ffbc70cafb45fafd79e5589c674a08389e275
Terese Emel <terese.emel@doctrin.se>
2024-07-07 19:42:31 +0200

    Topratedmovie shared step updated

--- cypress/support/step_definitions/topRatedMovieSteps.js
+++ cypress/support/step_definitions/topRatedMovieSteps.js
@@ -1,19 +1,7 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
-
-let ratingHref;
-
-
-When('I navigate to the "Top Box Office" section', () => {
-  cy.get('[data-testid="nav-link-category"] span')
-    .contains("Movies")
-    .click()
-    .get(".ipc-list-item__text")
-    .contains("Top Box Office")
-    .click(); 
-});
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Then('I see the "Top Box Office" list', () => {
-  cy.get(".ipc-metadata-list").should("be.visible"); 
+  cy.get(".ipc-metadata-list").should("be.visible");
 });
 
 When("I click on the second item in the list", () => {
@@ -23,35 +11,33 @@ When("I click on the second item in the list", () => {
 });
 
 When('I click on the "IMDb Rating" button', () => {
-  cy.get("[data-testid=hero-rating-bar__user-rating]")
-    .eq(1)
-    .click(); 
+  cy.get("[data-testid=hero-rating-bar__user-rating]").eq(1).click();
 });
 
 When('I click on the "Rate" button', () => {
- 
-cy.get("[data-testid=hero-rating-bar__user-rating__unrated]")
-  .contains("Rate")
-  .wait(5000)
-  .click({ force: true }); 
+  cy.get("[data-testid=hero-rating-bar__user-rating__unrated]")
+    .contains("Rate")
+    .wait(5000)
+    .click({ force: true });
 });
 
 Then("I see the rating modal", () => {
-  cy.get(".ipc-promptable-base__content").should("be.visible"); 
+  cy.get(".ipc-promptable-base__content").should("be.visible");
 });
 
 When("I set the rating to 5 stars", () => {
   cy.get(".ipc-starbar__rating__button")
     .eq(4)
     .click({ force: true })
-    .get(".ipc-rating-display__rating").contains("5"); 
+    .get(".ipc-rating-display__rating")
+    .contains("5");
 });
 
 When('I click on the "Rate" button in the modal', () => {
-  cy.get(".ipc-btn").contains("Rate").click({ force: true }); 
+  cy.get(".ipc-btn").contains("Rate").click({ force: true });
 });
 
 Then("I see a confirmation message for the rating", () => {
   // Add assertion to verify the confirmation message (might require waiting for the modal to close)
-cy.get(".ipc-promptable-base__content").should("not.be.visible"); 
+  cy.get(".ipc-promptable-base__content").should("not.be.visible");
 });
